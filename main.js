b97ad6453f197fbbbf6e5a7abf9bd10b07a68ca5
regalfyre <125704424+regalfyre@users.noreply.github.com>
2026-02-14 18:56:16 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 import * as THREE from "https://unpkg.com/three@0.159.0/build/three.module.js";
 import { PointerLockControls } from "https://unpkg.com/three@0.159.0/examples/jsm/controls/PointerLockControls.js";
 
